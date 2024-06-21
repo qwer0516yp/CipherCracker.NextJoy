8783c6669df0860d0d014c6e9154dddc0d346c0e
st_Akey <qwer0516yp@gmail.com>
2024-06-21 12:51:54 +1000

    add Google as the third Oauth provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,21 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Auth0 from "next-auth/providers/auth0";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Auth0,]
+  providers: [
+    GitHub, 
+    Auth0, 
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })]
 });
