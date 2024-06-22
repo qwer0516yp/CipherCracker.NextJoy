1e206287126fe574849072d02e49fab44686e769
st_Akey <qwer0516yp@gmail.com>
2024-06-22 19:28:18 +1000

    update sandbox to point to hash.js sample so people get better idea of a running playround is available there

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -4,10 +4,10 @@ import * as React from 'react';
 
 export default function App() {
   return (
-      <iframe src="https://codesandbox.io/p/devbox/dq39qc?migrateFrom=5sk7cr&embed=1" 
-        style={{width: '100%', height: '800px', border:'0', overflow:'hidden'}}
-        title="cryptojs"
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts" />
+    <iframe src="https://codesandbox.io/p/devbox/dq39qc?embed=1&file=%2Fsrc%2Fhash.js&migrateFrom=5sk7cr&showConsole=true"
+    style={{width: '100%', height: '800px', border:'0', overflow:'hidden'}}
+     title="cryptojs"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts" />
   );
 }
