01cd34a17a11b8df16f0cadf97a9c0492432c13a
st_Akey <qwer0516yp@gmail.com>
2025-03-07 21:51:06 +1100

    default iframe heights as 100% on jwt jwe jwk pages

--- src/app/jwt/page.tsx
+++ src/app/jwt/page.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 export default function App() {
   return (
     <iframe src="https://jwt.io/"
-    style={{width: '100%', height: '800px', border:'0', overflow:'hidden'}}
+    style={{width: '100%', height: '100%', border:'0', overflow:'hidden'}}
      title="jwt.io"
      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts" />
